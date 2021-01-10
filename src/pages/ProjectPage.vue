92adff3c617583a3b1eb1ebb3c3968ecd0cce4a5
dimarhanung <dimarhanung@ecampus.ut.ac.id>
2021-01-10 17:42:59 +0700

    fix: color search input

--- src/pages/ProjectPage.vue
+++ src/pages/ProjectPage.vue
@@ -5,7 +5,7 @@
       :items="projects"
       :keys="['title']"
       v-on:filtered="updateData($event)"
-      class="px-8"
+      class="px-8 text-black"
     />
     <div class="px-8 flex flex-wrap justify-center">
       <div
@@ -13,27 +13,27 @@
         :key="i"
         class="card-box p-4 w-1/4 my-0"
       >
-        <div class="shadow rounded-lg">
+        <div class="shadow bg-primary-card rounded-lg">
           <div class="border-b p-2">{{ project.title }}</div>
           <div class="flex flex-wrap p-2">
             <div
-              class="text-xs bg-green-200 text-green-700 px-2 m-1 rounded-full"
+              class="text-xs bg-primary-1 text-primary-active px-2 m-1 rounded-full"
               v-for="(category, i) in project.category"
               :key="'A' + i"
             >
               {{ category }}
             </div>
           </div>
-          <div class="flex">
+          <div class="flex rounded-b-lg bg-primary-3">
             <a
               target="_blank"
               :href="project.github"
-              class="text-xl p-2 bg-gray-200 rounded-bl-lg "
+              class="text-xl p-2 rounded-bl-lg "
             >
               <font-awesome-icon :icon="['fab', 'github']" />
             </a>
             <div
-              class="bg-blue-300 opacity-50 cursor-not-allowed select-none w-full grid place-items-center px-2 text-sm"
+              class="cursor-not-allowed select-none w-full grid place-items-center px-2 text-sm"
               title="Deskripsi belum siap"
             >
               Deskripsi
@@ -41,7 +41,7 @@
             <a
               target="_blank"
               :href="project.link"
-              class="bg-yellow-300 w-full grid place-items-center rounded-br-lg px-2 text-sm"
+              class="w-full grid place-items-center rounded-br-lg px-2 text-sm"
             >
               Demo
             </a>
